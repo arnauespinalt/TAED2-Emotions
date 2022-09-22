d76a875b89fd0dae70eb807be7346dabe8161914
DaniGmzGnz <danigomeez2011@gmail.com>
2022-09-22 14:58:55 +0200

    DEV-Created-Project-Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We are the team that develops a model to classify some text input into a variety of emotions.',
+    author='EmotionTeam',
+    license='MIT',
+)
