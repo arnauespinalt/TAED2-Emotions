286595a1b878b975d92f25fca9e430ad6d5ae081
onaclaraventosa <ona.clara@estudiantat.upc.edu>
2022-10-06 14:41:17 +0200

    Code-carbon

--- src/main.py
+++ src/main.py
@@ -4,11 +4,16 @@ from src.models.train_model import train_model
 from src.models.predict_model import predict_model
 from src.visualization.visualize import plots
 import logging
+from codecarbon import EmissionsTracker
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 def ETL():
+    # We will track CO2 emissions for each step of our pipeline
+    tracker = EmissionsTracker()
+    tracker.start()
+
     # Load and transform the dataset.
     dataset = load("emotion")
 
@@ -22,3 +27,5 @@ def ETL():
 
     # Do some visualization for the metrics.
     plots(metrics)
+    tracker.stop()
+    print(tracker)
