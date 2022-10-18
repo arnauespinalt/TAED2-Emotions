7b2e7b7112c25adca9ddaf4dfbba649dc1ae4278
DaniGmzGnz <danigomeez2011@gmail.com>
2022-10-18 13:02:29 +0200

    updated api

--- src/scripts/predict_model.py
+++ src/scripts/predict_model.py
@@ -16,11 +16,9 @@ def mlflow_metrics(y_val, y_pred):
     acc = accuracy_score(y_val, y_pred)
     rec = recall_score(y_val, y_pred, average="weighted")
 
-<<<<<<< HEAD
     return {"accuracy": acc, 
             "f1": f1, 
             "recall": rec}
-=======
     fpr, tpr, threshold = metrics.roc_curve(y_val, y_pred)
     roc_auc = metrics.auc(fpr, tpr)
 
@@ -30,17 +28,13 @@ def mlflow_metrics(y_val, y_pred):
             "roc_auc": roc_auc,
             "fpr": fpr,
             "tpr": tpr}
->>>>>>> 4bb5b3c41e84230055b293d6eb6557b4f90d97b5
+
 
 def predict_model(model, dataset):
 
     with mlflow.start_run():
         logger.info("Executing predictions.")
-<<<<<<< HEAD
-        print(dataset['validation'][0])
-=======
 
->>>>>>> 4bb5b3c41e84230055b293d6eb6557b4f90d97b5
         preds_output = model.predict(dataset["validation"])
         preds_output.metrics
 
